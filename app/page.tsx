2430b3fc92e27c99b2a05f25da336424ca05ae70
William Crane <wcran7@gmail.com>
2026-03-28 21:21:04 +1000

    Feat(BookCard): Got the book card component working, simple enough to get the layout and component working had to update divs on the page.tsx and allow external images through next.config for current book covers < Will update this in the future.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
-import HeroSection from "@/components/HeroSection";
 import React from "react";
+import HeroSection from "@/components/HeroSection";
+import { sampleBooks } from "@/lib/constants";
+import BookCard from "@/components/BookCard";
 
 const Page = () => {
   return (
-    <div>
-      <HeroSection />
-    </div>
+    <main>
+      <div className="wrapper">
+        <HeroSection />
+
+        <section className="library">
+          <div className="library-books-grid">
+            {sampleBooks.map((book) => (
+              <BookCard
+                key={book._id}
+                title={book.title}
+                author={book.author}
+                coverURL={book.coverURL}
+                slug={book.slug}
+              />
+            ))}
+          </div>
+        </section>
+      </div>
+    </main>
   );
 };
 
