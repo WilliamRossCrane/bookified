c59c4710994f89815c7c2bc736c694b1e55d02d2
William Crane <wcran7@gmail.com>
2026-04-28 18:28:51 +1000

    Fix(Vercel - Cache): Updated so books get refreshed automatically to appear and not waiting on cache

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -6,6 +6,7 @@ import { escapeRegex, generateSlug, serializeData } from "@/lib/utils";
 import Book from "@/database/models/book.model";
 import BookSegment from "@/database/models/book-segment.model";
 import mongoose from "mongoose";
+import { revalidatePath } from "next/cache";
 
 export const getAllBooks = async (search?: string) => {
   try {
@@ -95,6 +96,8 @@ export const createBook = async (data: CreateBook) => {
       slug,
       totalSegments: 0,
     });
+
+    revalidatePath("/");
 
     return {
       success: true,
